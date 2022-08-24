96ecd6055b90265edd71de77fdf25221b604ca44
Jayghosh Rao <jayghoshter@gmail.com>
2022-08-24 22:07:32 +0200

    update main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy)]
 struct Node {
     id: i32,
 }
@@ -27,13 +28,20 @@ impl UndirectedGraph{
 }
 
 fn main(){
-    let n1: Node = Node { id:1 , };
-    let n2: Node = Node { id:2 , };
 
-    let e1: UndirectedEdge = UndirectedEdge { end_nodes : (n1,n2), length:1.0 };
+    let n1: Node = Node { id:1 };
+    let n2: Node = Node { id:2 };
+    let n3: Node = Node { id:3 };
+    let n4: Node = Node { id:4 };
 
-    let g1: UndirectedGraph = UndirectedGraph { edges: vec![e1] };
+    let e1: UndirectedEdge = UndirectedEdge { end_nodes: (n1, n2), length: 1.0 };
+    let e2: UndirectedEdge = UndirectedEdge { end_nodes: (n1, n3), length: 1.0 };
+    let e3: UndirectedEdge = UndirectedEdge { end_nodes: (n2, n4), length: 1.0 };
+    let e4: UndirectedEdge = UndirectedEdge { end_nodes: (n3, n4), length: 2.0 };
+    let e5: UndirectedEdge = UndirectedEdge { end_nodes: (n3, n4), length: 2.0 };
 
-    g1.print()
+    let g1: UndirectedGraph = UndirectedGraph { edges: vec![e1,e2,e3,e4] };
+
+    g1.print();
 
 }
