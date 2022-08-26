0ca034f96a2c9c45efee8fe606ba0bbb99b2b052
Jayghosh Rao <jayghoshter@gmail.com>
2022-08-26 21:00:44 +0200

    remove Node. use i32 instead

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
-#[derive(Clone, Copy)]
-struct Node {
-    id: i32,
-}
-
 struct UndirectedEdge{
-    end_nodes: (Node, Node),
+    end_nodes: (i32, i32),
     length: f64
 }
 
 impl UndirectedEdge{
-    fn other_end(&self, n: Node) -> Node {
-        let leftid = self.end_nodes.0.id;
-        let rightid = self.end_nodes.1.id;
-        match n.id {
-            a if a == leftid => self.end_nodes.1,
-            b if b == rightid => self.end_nodes.0,
-            _ => panic!("Given node {} not part of edge {}--{}", n.id, leftid, rightid)
+    fn other_end(&self, n: i32) -> i32 {
+        match n {
+            a if a == self.end_nodes.0 => self.end_nodes.1,
+            b if b == self.end_nodes.1 => self.end_nodes.0,
+            _ => panic!("Given node {} not part of edge {:?}", n, self.end_nodes)
         }
     }
 
     fn is_adjacent(&self, other: UndirectedEdge) -> bool {
-        self.end_nodes.0.id == other.end_nodes.0.id ||
-            self.end_nodes.0.id == other.end_nodes.1.id ||
-            self.end_nodes.1.id == other.end_nodes.0.id ||
-            self.end_nodes.1.id == other.end_nodes.1.id 
+        self.end_nodes.0 == other.end_nodes.0 ||
+            self.end_nodes.0 == other.end_nodes.1 ||
+            self.end_nodes.1 == other.end_nodes.0 ||
+            self.end_nodes.1 == other.end_nodes.1 
     }
 }
 
@@ -32,15 +25,14 @@ struct UndirectedGraph{
 }
 
 struct UndirectedPath{
-    nodes: Vec<Node>,
-    length: f64
+    nodes: Vec<i32>,
 }
 
 impl UndirectedGraph{
     fn print(&self){
         print!("Graph with Edges: ");
         for i in 0..self.edges.len() {
-            print!("{}--{}, ", self.edges[i].end_nodes.0.id, self.edges[i].end_nodes.1.id);
+            print!("{}--{}, ", self.edges[i].end_nodes.0, self.edges[i].end_nodes.1);
         }
         println!("")
     }
@@ -48,29 +40,24 @@ impl UndirectedGraph{
 
 fn main(){
 
-    let n1: Node = Node { id:1 };
-    let n2: Node = Node { id:2 };
-    let n3: Node = Node { id:3 };
-    let n4: Node = Node { id:4 };
+    let e1: UndirectedEdge = UndirectedEdge { end_nodes: (1, 2), length: 1.0 };
+    let e2: UndirectedEdge = UndirectedEdge { end_nodes: (1, 3), length: 1.0 };
+    let e3: UndirectedEdge = UndirectedEdge { end_nodes: (2, 4), length: 1.0 };
+    let e4: UndirectedEdge = UndirectedEdge { end_nodes: (3, 4), length: 2.0 };
+    let e5: UndirectedEdge = UndirectedEdge { end_nodes: (3, 4), length: 2.0 };
 
-    let e1: UndirectedEdge = UndirectedEdge { end_nodes: (n1, n2), length: 1.0 };
-    let e2: UndirectedEdge = UndirectedEdge { end_nodes: (n1, n3), length: 1.0 };
-    let e3: UndirectedEdge = UndirectedEdge { end_nodes: (n2, n4), length: 1.0 };
-    let e4: UndirectedEdge = UndirectedEdge { end_nodes: (n3, n4), length: 2.0 };
-    let e5: UndirectedEdge = UndirectedEdge { end_nodes: (n3, n4), length: 2.0 };
-
-    let e6: UndirectedEdge = UndirectedEdge { end_nodes: (n3, n1), length: 2.0 };
-    let e7: UndirectedEdge = UndirectedEdge { end_nodes: (n1, n2), length: 2.0 };
+    let e6: UndirectedEdge = UndirectedEdge { end_nodes: (3, 1), length: 2.0 };
+    let e7: UndirectedEdge = UndirectedEdge { end_nodes: (1, 2), length: 2.0 };
 
     let g1: UndirectedGraph = UndirectedGraph { edges: vec![e1,e2,e3,e4] };
 
     g1.print();
 
-    println!("{}--{}", e5.end_nodes.0.id, e5.end_nodes.1.id);
+    println!("{}--{}", e5.end_nodes.0, e5.end_nodes.1);
 
-    // println!("{}", e5.other_end(n3).id);
-    // println!("{}", e5.other_end(n4).id);
-    // println!("{}", e5.other_end(n1).id);
+    // println!("{}", e5.other_end(3));
+    // println!("{}", e5.other_end(4));
+    // println!("{}", e5.other_end(1));
 
     println!("{}", e5.is_adjacent(e6));
     println!("{}", e5.is_adjacent(e7));
